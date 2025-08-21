ff8e8774d396067f5030d146a1d3eacbdb2763b0
Kevin Bedi <kevin.m.bedi@gmail.com>
2025-08-21 15:41:26 -0700

    Add convenient helper for creating spring configs at runtime

--- Springs/SpringConfig.cs
+++ Springs/SpringConfig.cs
@@ -13,5 +13,16 @@ namespace SimpleUnitySprings
 
         public float trailDelay = 0.1f;
 
+
+        public static SpringConfig Create(float tension = 600, float friction = 30, float mass = 1, float trailDelay = 0.1f)
+        {
+            var config = ScriptableObject.CreateInstance<SpringConfig>();
+            config.tension = tension;
+            config.friction = friction;
+            config.mass = mass;
+            config.trailDelay = trailDelay;
+            return config;
+        }
+
     }
 }
